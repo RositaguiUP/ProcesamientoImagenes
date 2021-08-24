42b5cf41a31d2991d88764aa8114f189214f22b8
Rosita <rositaguipla@gmail.com>
2021-08-23 23:35:56 -0500

    ecualizeImage & createHistogram

--- Histogram/Histogram/Main.cpp
+++ Histogram/Histogram/Main.cpp
@@ -7,38 +7,74 @@
 using namespace cv;
 using namespace std;
 
+int checkImage(Mat, string);
+Mat createHistogram(Mat, int[]);
+Mat ecualizeImage(Mat, int[]);
+
 int main()
 {
-    string originalPath = samples::findFile("images/monalisa.jpg");
-
+    string originalPath = samples::findFile("images/subImg.jpg");
     Mat originalImage = imread(originalPath, IMREAD_GRAYSCALE);
 
-    Mat rectImage(512, 512, CV_8UC1, Scalar(255));
-
-    if (originalImage.empty())
-    {
-        cout << "Could not read the image: " << originalPath << endl;
+    if (checkImage(originalImage, originalPath))
         return -1;
-    }
+    
+    //int* originalGrayScaleValues = getGrayScaleValues(originalImage);
 
-    int count[256] = {};
+    int originalGrayScaleValues[256] = {};
 
     for (int j = 0; j < originalImage.rows; j++) {
         for (int i = 0; i < originalImage.cols; i++) {
-            count[int(originalImage.at<uchar>(j, i))] += 1;
+            originalGrayScaleValues[int(originalImage.at<uchar>(j, i))] += 1;
         }
     }
 
-    for (int j = 0; j < rectImage.rows; j++) {
-        for (int i = 0; i < rectImage.cols; i++) {
-            rectImage.at<uchar>(j, i) = (uchar)(0);
+    Mat equalizedImage = ecualizeImage(originalImage, originalGrayScaleValues);
+    Mat originalHistogram = createHistogram(originalImage, originalGrayScaleValues);
+
+    int equalizedGrayScaleValues[256] = {};
+
+    for (int j = 0; j < equalizedImage.rows; j++) {
+        for (int i = 0; i < equalizedImage.cols; i++) {
+            equalizedGrayScaleValues[int(equalizedImage.at<uchar>(j, i))] += 1;
+        }
+    }
+
+    Mat ecualizedHistogram = createHistogram(equalizedImage, equalizedGrayScaleValues);
+    
+    imshow("Original", originalImage);
+    imshow("Ecualization", equalizedImage);
+    imshow("Original Histogram", originalHistogram);
+    imshow("Ecualized Histogram", ecualizedHistogram);
+    
+
+    waitKey(0); // Wait for a keystroke in the window
+    return 0;
+}
+
+int checkImage(Mat image, string path) {
+    if (image.empty())
+    {
+        cout << "Could not read the image: " << path << endl;
+        return -1;
+    }
+
+    return 0;
+}
+
+Mat createHistogram(Mat originalImage, int grayScaleValues[]) {
+    Mat histogramImage(512, 512, CV_8UC1, Scalar(255));
+
+    for (int j = 0; j < histogramImage.rows; j++) {
+        for (int i = 0; i < histogramImage.cols; i++) {
+            histogramImage.at<uchar>(j, i) = (uchar)(0);
         }
     }
 
     int max = 0;
     for (int i = 0; i < 256; i++)
-        if (max < count[i])
-            max = count[i];
+        if (max < grayScaleValues[i])
+            max = grayScaleValues[i];
 
     int imagePixels = originalImage.rows * originalImage.cols;
     int histogramPixels = 0;
@@ -46,28 +82,47 @@ int main()
     Point pnt1, pnt2;
 
     for (int i = 0; i < 256; i++) {
-        val = count[i];
+        val = grayScaleValues[i];
         y = 512 * val / max;
         y0 = 512 - y;
-        pnt1 = Point(i * 2+1, 512);
-        pnt2 = Point(i*2+1, y0);
-        rectangle(rectImage, pnt1, pnt2, Scalar(255, 255, 255), 5, LINE_8, 0);
+        pnt1 = Point(i * 2 + 1, 512);
+        pnt2 = Point(i * 2 + 1, y0);
+        rectangle(histogramImage, pnt1, pnt2, Scalar(255, 255, 255), 5, LINE_8, 0);
 
-        cout << i << ") " << val << "   \t";
+        // cout << i << ") " << val << "   \t";
         histogramPixels += val;
     }
 
-    cout << "\n\n\tHistogram Pixels = " << histogramPixels << "\n\tImage Pixels = " << imagePixels << endl;
+    // cout << "\n\n\tHistogram Pixels = " << histogramPixels << "\n\tImage Pixels = " << imagePixels << endl;
+    return histogramImage;
+}
 
-    //rectangle(rectImage, Point(50, 200), Point(250, 20), Scalar(255,255,255), 5, LINE_8, 0);
-    //rectangle(rectImage, Point(350, 300), Point(500, 500), Scalar(255, 255, 255), FILLED, LINE_8, 0);
+Mat ecualizeImage(Mat originalImage, int grayScaleValues[]) {
+    Mat equalizedImage(originalImage.rows, originalImage.cols, CV_8UC1, Scalar(255));
 
-    namedWindow("Original", WINDOW_AUTOSIZE); // Can configure the window
+    int imagePixels = originalImage.rows * originalImage.cols;
 
-    imshow("Original", rectImage);
+    float pmfValues[256] = {};
+    float cdfValues[256] = {};
 
-    
+    for (int i = 0; i < 256; i++) {
+        pmfValues[i] = (float)(grayScaleValues[i]) / imagePixels;
+        cdfValues[i] = pmfValues[i];
 
-    waitKey(0); // Wait for a keystroke in the window
-    return 0;
+        if (i != 0)
+            cdfValues[i] += cdfValues[i - 1];
+
+        // cout << "pmf: " << pmfValues[i] << "\tcdf" << cdfValues[i] << endl;
+    }
+
+    int grayTone;
+
+    for (int j = 0; j < originalImage.rows; j++) {
+        for (int i = 0; i < originalImage.cols; i++) {
+            grayTone = int(originalImage.at<uchar>(j, i));
+            equalizedImage.at<uchar>(j, i) = (uchar)floor((256 - 1) * (cdfValues[grayTone]));
+        }
+    }
+
+    return equalizedImage;
 }
