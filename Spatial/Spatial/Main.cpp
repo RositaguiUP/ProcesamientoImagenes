2c60a4a63603bda99f91870c33af5750c41e274b
Rosita <rositaguipla@gmail.com>
2021-09-22 14:30:49 -0500

    Added padding

--- Spatial/Spatial/Main.cpp
+++ Spatial/Spatial/Main.cpp
@@ -27,6 +27,9 @@ int main()
     Mat thresholdBluredImage(image1.rows, image1.cols, CV_8UC1, Scalar(255));
     Mat bluredVarGaussImage(image1.rows, image1.cols, CV_8UC1, Scalar(255));
 
+    /* Filtros espaciales*/
+
+#pragma region Blured Image
     int umbral = 128;
     int sum, prom;
     for (int j = 1; j < bluredImage.rows - 1; j++) {
@@ -50,8 +53,9 @@ int main()
             }
         }
     }
+#pragma endregion  
 
-
+#pragma region BluredVarImage
     int var = 5;
     int inter = var / 2;
     int interInf = 0 - inter;
@@ -69,7 +73,9 @@ int main()
             bluredVarImage.at<uchar>(j, i) = (uchar)(prom);
         }
     }
+#pragma endregion
 
+#pragma region Pimienta Media
     var = 3;
     int mid = (var * var) / 2;
     inter = var / 2;
@@ -89,6 +95,9 @@ int main()
         }
     }
 
+#pragma endregion
+
+#pragma region BluredGaussImage
     int tot = 159;
     int sum2 = 0;
     var = 5;
@@ -96,16 +105,16 @@ int main()
     interInf = 0 - inter;
     interSup = 1 + inter;
     int mask[5][5] = { {2, 4, 5, 4, 2}, {4, 9, 12, 9, 4}, {5, 12, 15, 12, 5}, {4, 9, 12, 9, 4}, {2, 4, 5, 4, 2} };
-        // { {1, 2, 1 }, {2, 4, 2 }, {1, 2, 1 }}; 16 3x3
-        // { {1, 4, 7, 4, 1 }, {4, 16, 26, 16, 4}, {7, 26, 41, 26, 7}, {4, 16, 26, 16, 4}, {1, 4, 7, 4, 1 } }; 273 5x5
-        // { {2, 4, 5, 4, 2}, {4, 9, 12, 9, 4}, {5, 12, 15, 12, 5}, {4, 9, 12, 9, 4}, {2, 4, 5, 4, 2}}; 159 5x5
+    // { {1, 2, 1 }, {2, 4, 2 }, {1, 2, 1 }}; 16 3x3
+    // { {1, 4, 7, 4, 1 }, {4, 16, 26, 16, 4}, {7, 26, 41, 26, 7}, {4, 16, 26, 16, 4}, {1, 4, 7, 4, 1 } }; 273 5x5
+    // { {2, 4, 5, 4, 2}, {4, 9, 12, 9, 4}, {5, 12, 15, 12, 5}, {4, 9, 12, 9, 4}, {2, 4, 5, 4, 2}}; 159 5x5
     for (int j = inter; j < bluredGaussImage.rows - inter; j++) {
         for (int i = inter; i < bluredGaussImage.cols - inter; i++) {
             sum = 0;
             sum2 = 0;
             for (int l = interInf; l < interSup; l++) {
                 for (int k = interInf; k < interSup; k++) {
-                    sum += image1.at<uchar>(j + l, i + k) * mask[interSup+l-1][interSup+k-1];
+                    sum += image1.at<uchar>(j + l, i + k) * mask[interSup + l - 1][interSup + k - 1];
                     sum2 += thresholdImage.at<uchar>(j + l, i + k) * mask[interSup + l - 1][interSup + k - 1];
                 }
             }
@@ -115,15 +124,16 @@ int main()
             thresholdBluredImage.at<uchar>(j, i) = (uchar)(prom);
         }
     }
+#pragma endregion
 
-
+#pragma region BluredGaussImage with Sigma
 
     int n, m, x0, y0, x, y, sigma = 24;
     n = 9;
     m = 9;
     x0 = floor(m / (float)2);
     y0 = floor(n / (float)2);
-    
+
     float Texp, tconst;
     Mat gaussMask(n, m, CV_32F);
 
@@ -157,16 +167,44 @@ int main()
             sumMask = 0;
             for (int l = interInf; l < interSup; l++) {
                 for (int k = interInf; k < interSup; k++) {
-                    sumMask += image1.at<uchar>(j + l, i + k) * gaussMask.at<float>(interSup + l - 1,interSup + k - 1);
+                    sumMask += image1.at<uchar>(j + l, i + k) * gaussMask.at<float>(interSup + l - 1, interSup + k - 1);
                 }
             }
             prom = sumMask / maskTot;
             bluredVarGaussImage.at<uchar>(j, i) = (uchar)(prom);
         }
     }
+#pragma endregion
+
+    /* Filtro espacial con padding*/
+    Mat paddingImage(image2.rows + 10, image2.cols + 10, CV_8U, Scalar(0));
+    Mat zona = paddingImage.colRange(5, (5 + image2.cols)).rowRange(5, (5 + image2.rows));
+
+    image2.copyTo(zona);
+
+    var = 3;
+    mid = (var * var) / 2;
+    inter = var / 2;
+    interInf = 0 - inter;
+    interSup = 1 + inter;
+    vecPixels.clear();
+    for (int j = inter; j < paddingImage.rows - inter; j++) {
+        for (int i = inter; i < paddingImage.cols - inter; i++) {
+            vecPixels.clear();
+            for (int l = interInf; l < interSup; l++) {
+                for (int k = interInf; k < interSup; k++) {
+                    vecPixels.push_back(paddingImage.at<uchar>(j + l, i + k));
+                }
+            }
+            sort(vecPixels.begin(), vecPixels.end());
+            paddingImage.at<uchar>(j, i) = (uchar)(vecPixels[mid]);
+        }
+    }
+
+    zona.copyTo(image2);
 
 
-    imshow("Original", image1);
+    // imshow("Original", image1);
     // imshow("Blur 3x3", bluredImage);
     // imshow("Blur Variable (5x5)", bluredVarImage);
 
@@ -177,7 +215,13 @@ int main()
     // imshow("Threshold", thresholdImage);
     // imshow("Threshold Gauss Blur 5x5", thresholdBluredImage);
 
-    imshow("Var Gauss 9x9", bluredVarGaussImage);
+    // imshow("Var Gauss 9x9", bluredVarGaussImage);
+    
+    // imshow("Original", image2);
+    imshow("Image with padding", paddingImage);
+    imshow("Image with padding and filter", paddingImage);
+    imshow("Image without padding and filter", image2);
+
 
     waitKey(0); // Wait for a keystroke in the window
     return 0;
