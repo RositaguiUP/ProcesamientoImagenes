74d4c1ad8a8240bca7ba31ab12cf9c29180b3ee2
Rosita <65567638+RositaguiUP@users.noreply.github.com>
2021-10-20 14:33:35 -0500

    Laplace

--- Bordes/Bordes/Main.cpp
+++ Bordes/Bordes/Main.cpp
@@ -7,19 +7,42 @@
 using namespace cv;
 using namespace std;
 
-Mat bordes(Mat original);
+Mat bordes(Mat);
+Mat threshold(Mat, int);
+Mat laplace(Mat);
 
 int main() {
 	string path = samples::findFile("images/monalisa.jpg");
 	Mat originalImage = imread(path, IMREAD_GRAYSCALE);
 
 	Mat bordesImage = bordes(originalImage);
+	Mat thresholdImage = threshold(bordesImage, 95);
+	Mat laplaceImage = laplace(originalImage);
 
 	imshow("Original", originalImage);
-	imshow("Bordes", bordesImage);
+	//imshow("Bordes", bordesImage);
+	//imshow("Threshold", thresholdImage);
+	imshow("Laplace", laplaceImage);
 
 	int k = waitKey(0); // Wait for a keystroke in the window
 	return 0;
+}
+
+Mat threshold(Mat original, int umbral) {
+	Mat newImage(original.rows, original.cols, CV_8UC1);
+
+	for (int j = 0; j < original.rows; j++) {
+		for (int i = 0; i < original.cols; i++) {
+			if (original.at<uchar>(j, i) > umbral) {
+				newImage.at<uchar>(j, i) = (uchar)(255);
+			}
+			else {
+				newImage.at<uchar>(j, i) = (uchar)(0);
+			}
+		}
+	}
+
+	return newImage;
 }
 
 Mat bordes(Mat original) {
@@ -48,8 +71,43 @@ Mat bordes(Mat original) {
 		}
 	}
 
-	imshow("gx", gx);
-	imshow("gy", gy);
+	//imshow("gx", gx);
+	//imshow("gy", gy);
+
+	return newImage;
+}
+
+Mat laplace(Mat original) {
+	Mat newImage(original.rows, original.cols, CV_8UC1);
+
+	int sum;
+	int mask[3][3] = { {0, 1, 0}, {1, -4, 1}, {0, 1, 0} };
+
+	double lapmin, lapmax;
+
+	for (int j = 1; j < original.rows - 1; j++) {
+		for (int i = 1; i < original.cols - 1; i++) {
+			sum = 0;
+			for (int l = -1; l <= 1; l++) {
+				for (int k = -1; k <= 1; k++) {
+					sum += original.at<uchar>(j + l, i + k) * mask[l + 1][k + 1];
+				}
+			}
+
+			newImage.at<uchar>(j, i) = (uchar)(abs(sum));
+		}
+	}
+
+	minMaxLoc(newImage, &lapmin, &lapmax);
+
+	for (int j = 1; j < original.rows - 1; j++) {
+		for (int i = 1; i < original.cols - 1; i++) {
+
+			newImage.at<uchar>(j, i) = (uchar)((newImage.at<uchar>(j, i) * 255) / lapmax);
+		}
+	}
+
+	
 
 	return newImage;
 }
