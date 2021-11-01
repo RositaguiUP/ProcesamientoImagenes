d1e6d26514bf0db9203b223da34880777f951d7d
Rosita <65567638+RositaguiUP@users.noreply.github.com>
2021-11-01 14:30:04 -0600

    Rotation and translation

--- GeoTrans/GeoTrans/Main.cpp
+++ GeoTrans/GeoTrans/Main.cpp
@@ -13,17 +13,26 @@ using namespace std;
 
 Mat scale(Mat, float, float);
 Mat interpol(Mat, float, float);
+int interpolationP(Mat, float, float);
+Mat translate(Mat, int, int);
+Mat rotate(Mat, float);
+
 
 int main() {
 	string path = samples::findFile("images/cat.jpg");
 	Mat originalImage = imread(path, IMREAD_GRAYSCALE);
 
-	Mat reducImage = scale(originalImage, 0.5, 0.5);
-	Mat ampliImage = scale(originalImage, 1.5, 1.5);
+	// Mat reducImage = scale(originalImage, 0.5, 0.5);
+	//Mat ampliImage = scale(originalImage, 1.5, 1.5);
+
+	//Mat reducInterImage = interpol(originalImage, 0.5, 0.5);
+	//Mat ampliInterImage = interpol(originalImage, 1.5, 1.5);
+
+	Mat translImage = translate(originalImage, 200, 200);
+	//Mat rotateImage = rotate(originalImage, 90);
 
 	imshow("Original", originalImage);
-	imshow("Reducción", reducImage);
-	imshow("Ampliación", ampliImage);
+	imshow("Translation", translImage);
 
 	int k = waitKey(0); // Wait for a keystroke in the window
 	/* if (k == 's')
@@ -80,21 +89,110 @@ Mat interpol(Mat original, float cx, float cy) {
 				p = floor(((r2 - r1) / (y2 - y1)) * (u - y1) + r1);
 			}
 			else if (x1 == x2 && y1 != y2) {
-				p = floor((x1 / (y2 - y1)) * (u - y1) + r1);
+				q11 = original.at<uchar>(y1, v);
+				q12 = original.at<uchar>(y2, v);
+
+				p = floor((q12 - q11 / (y2 - y1)) * (u - y1) + q11);
 			}
 			else if (x1 != x2 && y1 == y2) {
-				q21 = original.at<uchar>(y1, x2);
-				q22 = original.at<uchar>(y2, x2);
+				q11 = original.at<uchar>(u, x1);
+				q12 = original.at<uchar>(u, x2);
 
-				r1 = ((q21 - q11) / (x2 - x1)) * (v - x1) + q11;
-
-				p = floor((r1 / (y2 - y1)) * (u - y1) + r1);
+				p = floor((q12 - q11 / (x2 - x1)) * (v - x1) + q11);
 			}
-			 
+			else {
+				p = original.at<uchar>(u, v);
+			}
 
-			newImage.at<uchar>(j, i) = original.at<uchar>(u, v);
+			newImage.at<uchar>(j, i) =(uchar)p;
 		}
 	}
 
 	return newImage;
+}
+
+int interpolationP(Mat original, float u, float v) {
+	float q11, q12, q21, q22, r1, r2;
+	int x1, x2, y1, y2, p;
+
+	x1 = floor(v);
+	x2 = ceil(v);
+	y1 = floor(u);
+	y2 = ceil(u);
+
+	if (x1 != x2 && y1 != y2) {
+		q11 = original.at<uchar>(y1, x1);
+		q12 = original.at<uchar>(y2, x1);
+		q21 = original.at<uchar>(y1, x2);
+		q22 = original.at<uchar>(y2, x2);
+
+		r1 = ((q21 - q11) / (x2 - x1)) * (v - x1) + q11;
+		r2 = ((q22 - q12) / (x2 - x1)) * (v - x1) + q12;
+
+		p = floor(((r2 - r1) / (y2 - y1)) * (u - y1) + r1);
+	}
+	else if (x1 == x2 && y1 != y2) {
+		q11 = original.at<uchar>(y1, v);
+		q12 = original.at<uchar>(y2, v);
+
+		p = floor((q12 - q11 / (y2 - y1)) * (u - y1) + q11);
+	}
+	else if (x1 != x2 && y1 == y2) {
+		q11 = original.at<uchar>(u, x1);
+		q12 = original.at<uchar>(u, x2);
+
+		p = floor((q12 - q11 / (x2 - x1)) * (v - x1) + q11);
+	}
+	else {
+		p = original.at<uchar>(u, v);
+	}
+
+	return p;
+}
+
+Mat translate(Mat original, int tx, int ty) {
+	int u, v;
+
+	Mat newImage(original.rows, original.cols, CV_8UC1);
+
+	for (int j = 0; j < newImage.rows; j++) {
+		for (int i = 0; i < newImage.cols; i++) {
+			u = j - ty;
+			v = i - tx;
+			if (u > 0 && u < newImage.rows && v > 0 && v < newImage.cols)
+				newImage.at<uchar>(j, i) = original.at<uchar>(u, v);
+		}
+	}
+
+	return newImage;
+}
+
+Mat rotate(Mat original, float teta) {
+	float u, v, u0, v0, x0, y0, q11, q12, q21, q22, r1, r2;
+	float radians = (teta * 3.14159) / 180;
+	int x1, x2, y1, y2, p;
+	Mat newImage(original.rows, original.cols, CV_8UC1);
+
+	for (int j = 0; j < newImage.rows; j++) {
+		for (int i = 0; i < newImage.cols; i++) {
+			x0 = i - original.cols / 2;
+			y0 = (original.rows / 2) - j;
+
+			v0 = (x0 * cos(radians)) + (y0 * sin(radians));
+			u0 = -(x0 * sin(radians)) + (y0 * cos(radians));
+
+			v = v0 + original.cols / 2;
+			u = (original.rows / 2) - u0;
+			if (u > 0 && u < original.rows && v > 0 && v < original.cols-1) {
+				p = interpolationP(original, u, v);
+				newImage.at<uchar>(j, i) = (uchar)p;
+			}
+			else {
+				newImage.at<uchar>(j, i) = (uchar)0;
+			}
+		}
+	}
+
+	return newImage;
+
 }
